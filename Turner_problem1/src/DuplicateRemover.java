f2cf4bdb022cef91f0fa4b1e59a7aceae735e5dd
Spike <gflamethrower@gmail.com>
2020-03-28 20:00:05 -0400

    first commit

---
+++ Turner_problem1/src/DuplicateRemover.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DuplicateRemover {
+	
+	private ArrayList<String> uniqueWords;
+
+	
+	DuplicateRemover(){
+		uniqueWords = new ArrayList<String>();
+	}
+	
+	public void remove(String dataFile) {
+		Scanner in = null;
+		String tmpWord;
+		
+		try {
+			in = new Scanner(Paths.get(dataFile));
+		} catch (IOException e) {			
+			System.out.println("Sorry, but problem1.txt was not found");
+			System.exit(404);
+		}
+		
+		while(in.hasNext()) {
+			tmpWord = in.next();
+			boolean duplicate = false;
+			for (String word: uniqueWords) {
+				if(word.toLowerCase().matches(tmpWord.toLowerCase())) {
+					duplicate = true;
+					break;
+				}
+			}
+			if(!duplicate) {
+				uniqueWords.add(tmpWord);
+			}
+		}					
+		in.close();
+	}
+	
+	public void write(String outputFile) {		
+		try {
+			FileWriter writer = new FileWriter(outputFile);
+			for(int i = 0; i < uniqueWords.size(); i++) {
+				writer.write(uniqueWords.get(i) + "\n");
+			}				
+			writer.close();				
+		} 
+		catch (IOException e) {			
+			try {
+				File newFile = new File(outputFile);				
+				newFile.createNewFile();					
+			} 				
+			catch (IOException e1) {
+				System.out.println("Could neither find, nor create file:" + outputFile);
+				e1.printStackTrace();
+				System.exit(405);
+			}						
+		}
+	}
+}
